9ef5087c1c40069712358888b593434278b5f6f2
hiroshi shimoda <hilolih@gmail.com>
2015-02-24 13:23:33 +0900

    add qmid, ql on runmqsc

--- lib/serverspec/type/runmqsc.rb
+++ lib/serverspec/type/runmqsc.rb
@@ -1,8 +1,8 @@
 require "pry"
 module Serverspec::Type
   class Runmqsc < Base
-    def status
-      @runner.get_runmqsc_status(@name).stdout.strip
+    %w{conname status channel qmid put}.each do |meth| 
+      define_method(meth){@runner.send("get_runmqsc_#{meth}", @name).stdout.strip}
     end
 
     def counts
